b97d537e6831f73ee27f4c2b7166467c25517ee5
Alex Marshall <alex.marshall@gmail.com>
2021-01-17 00:26:02 -0500

    updated secret, still borken, needs to be fixed

--- do-the-plex.sh
+++ do-the-plex.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 echo "Plexing now..."
 
-API_TOKEN_CLEAN=qzzCv5bMQHsy_zihsM74
-API_TOKEN_OPAQUE=$(echo -n ${API_TOKEN_CLEAN} | base64)
+export API_TOKEN_CLEAN=qzzCv5bMQHsy_zihsM74
+export API_TOKEN_OPAQUE=$(echo -n ${API_TOKEN_CLEAN} | base64)
 
 cat README.md
 kubectl apply -f ns-plex.yaml
+kubectl apply -f secret-plex.yaml
 kubectl apply -f pv-plex-config.yaml
 kubectl apply -f pv-plex-media.yaml
 kubectl apply -f service-tcp-plex.yaml
